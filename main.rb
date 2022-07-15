c8852e89d2f0417cc5f3281971ee76637156b5d3
KostiaFed <kostiantyn.fed@gmail.com>
2022-07-15 15:59:39 +0300

    changes

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-       bot.api.send_message(chat_id: message.chat.id, text: "Привіт, #{message.from.first_name}")
+       bot.api.send_message(chat_id: message.chat.id, text: "Привіт, #{message.from.first_name}, розробка гри вже розпочалась!")
     end
   end
 end
