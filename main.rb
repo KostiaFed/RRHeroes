577c6874056c157d1960b2f60c36f55aab389828
KostiaFed <kostiantyn.fed@gmail.com>
2022-07-15 15:41:57 +0300

    Initial commit

---
+++ main.rb
@@ -0,0 +1,12 @@
+require 'telegram/bot'
+
+TOKEN = '5501901022:AAHh76BZr6BYcX1A05axPKGbadYKmQjs9us'
+
+Telegram::Bot::Client.run(TOKEN) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+       bot.api.send_message(chat_id: message.chat.id, text: "Привіт, #{message.from.first_name}")
+    end
+  end
+end
