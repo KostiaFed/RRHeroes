01ae650db155b2eb218b567d1b97d7d5f69b2303
KostiaFed <kostiantyn.fed@gmail.com>
2022-07-15 16:05:16 +0300

    image test

--- main.rb
+++ main.rb
@@ -7,6 +7,8 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
     case message.text
     when '/start'
        bot.api.send_message(chat_id: message.chat.id, text: "Привіт, #{message.from.first_name}, розробка гри вже розпочалась!")
+    when '/photo'
+      bot.api.send_photo(chat_id: message.chat.id, photo: Faraday::UploadIO.new('001.png', 'image/png'))
     end
   end
 end
