2371f5c77315cdf86e283cf5b99c275c9f8b5c30
KostiaFed <kostiantyn.fed@gmail.com>
2022-08-08 20:50:02 +0300

    database initilization

--- telegram/main.rb
+++ telegram/main.rb
@@ -1,9 +1,16 @@
+require File.expand_path('..config/environment', __dir__)
 require 'telegram/bot'
 
 TOKEN = '5501901022:AAHh76BZr6BYcX1A05axPKGbadYKmQjs9us'
 
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
+
+  if !Player.exists?(telegram_id: message.from.id)
+    player = Player.create(telegram_id: message.from.id, name: message.from.first_name)
+  else
+    player = Player.find_by(telegram_id: message.from.id)
+  end
     case message.text
     when '/start'
        bot.api.send_message(chat_id: message.chat.id, text: "Привіт, #{message.from.first_name}, розробка гри вже розпочалась!")
